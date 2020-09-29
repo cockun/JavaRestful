25b3c2642bf0c0d39ccff8c11ec9311fb6ab78e1
nhai <thengansax612@sgmail.com>
2020-09-29 15:46:12 +0700

    d

--- Web Sach/index/js/jsUser/data.js
+++ Web Sach/index/js/jsUser/data.js
@@ -1,19 +1,37 @@
-
-$( document ).ready(function()
-{
-    $(function(){
-        $.ajax({    
-            type: 'GET',
-            url:'http://localhost:8080/products',
-            success : function(data)
-            {
-                var coc = data;
-                for(i=0;i<=coc.data.length;i++)
-                {
-                    console.log(coc.data.name);
+var placeAdd = document.getElementsByClassName("placeAdd")[0];
+$(document).ready(function () {
+  $(function () {
+    $.ajax({
+      type: "GET",
+      url: "http://localhost:8080/products",
+      success: function (datas) {
+        datas.data.forEach(data => {
+            console.log(data);
+            $("#placeAdd").append(
+                `<div class="productD">
+                <a href="single.html">
+                <div class="inner_content clearfix">
+                <div class="product_image">
+                    <img src="${data.pic}" alt=""/>
+                </div>
                 
-                }
-        }
-    })
-    })
-})
+                <div class="price">
+                   <div class="cart-left">  
+                        <p class="title">${data.name}</p>
+                        <div class="price1">
+                          <span class="actual">${data.discount}</span>
+                        </div>
+                    </div>
+                    <div class="cart-right"> </div>
+                    <div class="clear"></div>
+                 </div>
+               </div>
+               </a>
+            </div>`
+            )
+           
+        });
+      },
+    });
+  });
+});
