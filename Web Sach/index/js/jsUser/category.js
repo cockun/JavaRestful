56cd0a145f14e3f2b2215dfdc03d932c9bd8319f
nhai <thengansax612@sgmail.com>
2020-10-02 16:46:25 +0700

    d

--- Web Sach/index/js/jsUser/category.js
+++ Web Sach/index/js/jsUser/category.js
@@ -1,59 +1,46 @@
 var queryDict = {}
+location.search.substr(1).split("&").forEach(function(item) {queryDict[item.split("=")[0]] = item.split("=")[1]});
+var placeAdd = document.getElementsByClassName("placeAdd")[0];
 
-location.search.substr(1).split("&").forEach(function(item) {queryDict[item.split("=")[0]] = item.split("=")[1]})
+
 
 $(document).ready(function () {
-    $(function () {
-      $.ajax({
-        async: false,
-        type: "GET",
-        url: `http://localhost:8080/account?id=${queryDict.id}`,
-        success: function (datas) {
-             let data=datas.data;
-              $("#placeAdd").append(
-                `
-                <div class="contentCont">
-                <div class="infoUser Left">
-                    <div class="subInfo">
-                        <div class="info">ID</div>
-                        <input type="text" id="id" class="inputField" value="${data.id}">
-                    </div> 
-                    <div class="subInfo">
-                        <div class="info">Họ và tên</div>
-                        <input type="text" id="name" class="inputField" value="${data.name}">
-                    </div>
-                    <div class="subInfo">
-                        <div class="info">Số điện thoại</div>
-                        <input type="text" id="phone" class="inputField" value="${data.phone}">
-                    </div>
-                    <div class="subInfo">
-                        <div class="info"> Địa chỉ</div>
-                        <input type="text" id="address" class="inputField" value=${data.address}>
-                    </div>
-                    
-                   
-                </div>
-                <div class="infoUser Right">
-                <div class="subInfo">
-                <div class="info">Địa chỉ Email</div>
-                <input type="text" id="email"  class="inputField" value=${data.email}>
-            </div>
-            <div class="subInfo">
-           
-           
-            
-        </div>
-                    
-                    
-                    <div class="btnAccept" onclick="adjustAccount()" >Xác Nhận</div>
-                </div>
-            </div>
-    `
-              )
-        },
-      });
-    });
-    
 
+  $(function () {
+    $.ajax({
+      type: "GET",
+      url: `http://localhost:8080/search/products?field=idcategory&value=${queryDict.id}`,
+      success: function (datas) {
+        datas.data.forEach(data => {
+            console.log(data);
+            $("#placeAdd").append(
+                `<div class="productD">
+                <a href="single.html?id=${data.id}">
+                <div class="inner_content clearfix">
+                <div class="product_image">
+                    <img src="${data.pic}" alt=""/>
+                </div>
+                
+                <div class="price">
+                   <div class="cart-left">  
+                        <p class="title">${data.name}</p>
+                        <div class="price1">
+                          <span class="actual">${data.discount}</span>
+                        </div>
+                    </div>
+                    <div class="cart-right"> </div>
+                    <div class="clear"></div>
+                 </div>
+               </div>
+               </a>
+            </div>`
+            )
+           
+        });
+      },
+
+      
+    });
+  });
 });
 
